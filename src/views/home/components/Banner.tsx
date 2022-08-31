25a96a61e100eaa96429279b47ddb3c990a2bab3
0xsaranonearth <saran.s@pillow.fund>
2022-08-31 22:49:36 +0530

    Fix: Banner overflow

--- src/views/home/components/Banner.tsx
+++ src/views/home/components/Banner.tsx
@@ -7,12 +7,12 @@ type Props = {};
 const Banner = (props: Props) => {
   return (
     <Flex p={30} bg={"flash"} height={["40vh", "50vh", "70vh"]} flexDirection={"column"} justifyContent={"flex-end"}>
-      <Box height={[250, 300, 400]} mb={25}>
-        <Image width={[100, 160, 200]} height={[100, 160, 200]} src={Logo} />
-        <Text fontSize={30} letterSpacing={3} fontWeight={"bold"} color={"dark"}>
+      <Box height={[200, 300, 400]} mt={20} mb={25}>
+        <Image width={[80, 160, 200]} height={[80, 160, 200]} src={Logo} />
+        <Text fontSize={[20, 30, 30]} letterSpacing={3} fontWeight={"bold"} color={"dark"}>
           Tranzo Protocol
         </Text>
-        <Text letterSpacing={1} fontSize={[16, 18, 20]} color={"dark"} width={["100%", "40%", "40%"]}>
+        <Text letterSpacing={1} fontSize={[14, 18, 20]} color={"dark"} width={["100%", "40%", "40%"]}>
           Transfer your Aave positions from one address to another seamlessly.{" "}
         </Text>
       </Box>
