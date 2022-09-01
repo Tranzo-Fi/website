7b12a7e090375b29e28b116fe90c9527d63ba6ae
0xsaranonearth <saran.s@pillow.fund>
2022-09-01 23:29:44 +0530

    Add: Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import Home from "./views/home";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./app/utils/theme";
 import GlobalStyle from "./utils/globalStyle";
+import ReactGA from "react-ga";
 
+ReactGA.initialize("G-7DK957Q1KS");
+ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return (
     <ThemeProvider theme={theme}>
